f87789f4b20741b966ee9828287d61e681765311
gerolddelacruz <gldelacruz3@up.edu.ph>
2024-04-17 17:28:09 +0800

    Created match recordings and match summary

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get("/waiting_room", function(req, res) {
     res.render("waiting_room");
 });
 
+app.get("/match_summary", function(req, res) {
+    res.render("match_summary");
+});
+
+app.get("/match_recordings", function(req, res) {
+    res.render("match_recordings");
+});
+
 
 
 app.listen(8000, function() {
