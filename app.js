013c6ce6a184d05490da1b642582ae418ce6322d
gerolddelacruz <gldelacruz3@up.edu.ph>
2024-04-17 16:04:25 +0800

    Created waiting room

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/waiting_room", function(req, res) {
+    res.render("waiting_room");
+});
+
+
+
 app.listen(8000, function() {
     console.log("Listening to Port 8000");
 });
