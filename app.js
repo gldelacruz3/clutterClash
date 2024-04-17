818454ba387de7e4a6c7c430a32dd646eb5812b4
gerolddelacruz <gldelacruz3@up.edu.ph>
2024-04-17 14:27:00 +0800

    Created basic server, homepage, and css

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// This is a test comment
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/assets"));
+app.set("views", __dirname + "/views"); 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.listen(8000, function() {
+    console.log("Listening to Port 8000");
+});
