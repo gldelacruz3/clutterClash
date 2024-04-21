270b9f4908743588f3a8200c39b0ab67316361b8
gerolddelacruz <gldelacruz3@up.edu.ph>
2024-04-21 16:21:34 +0800

    added timer and scoring

--- app.js
+++ app.js
@@ -8,33 +8,76 @@ app.use(express.static(__dirname + "/assets"));
 app.set("views", __dirname + "/views"); 
 app.set("view engine", "ejs");
 
-var cleanerScore = 0;
-var distractorScore = 0;
+var clutter = {
+    "clutter1": { x: 57, y: 260 },
+    "clutter2": { x: 57, y: 390 },
+    "clutter3": { x: 57, y: 520 },
+    "clutter4": { x: 475, y: 275 },
+    "clutter5": { x: 600, y: 275 },
+    "clutter6": { x: 725, y: 275 },
+    "clutter7": { x: 740, y: 50 },
+    "clutter8": { x: 961, y: 40 },
+    "clutter9": { x: 1098, y: 40 },
+    "clutter10": { x: 1162, y:515 },
+    "clutter11": { x: 262, y: 130 },
+    "clutter12": { x: 262, y: 260 },
+    "clutter13": { x: 262, y: 390 },
+    "clutter14": { x: 262, y: 520 },
+    "clutter15": { x: 475, y: 538 },
+    "clutter16": { x: 600, y: 538 },
+    "clutter17": { x: 725, y: 538 },
+    "clutter18": { x: 961, y: 228 },
+    "clutter19": { x: 1098, y: 228 },
+    "clutter20": { x: 966, y: 515 }
+};
+
+startMatch();
 
 io.on("connection", function (socket) {
+    socket.emit("initializeClutter", clutter);
+    
     
     socket.on("dragging", function (moveData) {
+        clutter[moveData.clutterId].x = moveData.x;
+        clutter[moveData.clutterId].y = moveData.y;
         socket.broadcast.emit("moveElement", moveData);
     });
 
     socket.on("drop", function (moveData) {
+        clutter[moveData.clutterId].x = moveData.x;
+        clutter[moveData.clutterId].y = moveData.y;
         socket.broadcast.emit("moveElement", moveData);
-
+        updateScore();
     });
-
-
 });
 
-function updateScore(moveData) {
-    let x = moveData.x;
-    let y = moveData.y;
-
-    if((x >= 10 && x <= 210 && y >= 165) || (x >= 665 && x <= 850 && y <= 126) || (x >= 385 && x <= 850 && y > 126 && y <= 496) || (x >= 860 && x <= 1270 && y <= 175) || (x >= 1088 && x <= 1270 && y >= 450 && y <= 650)) {
-        cleanerScore += 1;    
-    } else {
-        distractorScore += 2;
+function updateScore() {
+    let cleanerScore = 0;
+    let distractorScore = 0;
+    for (let id in clutter) {
+        let x = clutter[id].x;
+        let y = clutter[id].y;
+        if((x >= 10 && x <= 210 && y >= 165) || (x >= 665 && x <= 850 && y <= 126) || (x >= 385 && x <= 850 && y > 126 && y <= 496) || (x >= 860 && x <= 1270 && y <= 175) || (x >= 1088 && x <= 1270 && y >= 415 && y <= 650)) {
+            cleanerScore += 1; 
+        } else {
+            distractorScore += 1;
+        }
     }
+    cleanerScore = (cleanerScore - 10 < 0)? 0: cleanerScore - 10;
+    distractorScore = (distractorScore - 10 < 0)? 0: distractorScore - 10;
     console.log(cleanerScore + " | " + distractorScore);
+}
+
+function startMatch() {
+    let matchDuration = 15;
+    let intervalId = setInterval(function() {
+            matchDuration = matchDuration - 1;
+            io.emit("updateMatchTimer", matchDuration);
+            if (matchDuration == 0) {
+                clearInterval(intervalId);
+            } 
+        }, 1000
+    );
 }
 
 app.get("/", function(req, res) {
--- assets/scripts/live_match.js
+++ assets/scripts/live_match.js
@@ -1,47 +1,68 @@
 const socket = io();
 
-socket.on("moveElement", function(moveData) {
-    document.getElementById(moveData.clutterId).style.left = moveData.x + "px";
-    document.getElementById(moveData.clutterId).style.top = moveData.y + "px";
-});
+var matchTime = 120;
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var clutterElements = document.querySelectorAll(".clutter");
+
+    socket.on("moveElement", function(moveData) {
+        document.getElementById(moveData.clutterId).style.left = moveData.x + "px";
+        document.getElementById(moveData.clutterId).style.top = moveData.y + "px";
+    });
+
+    socket.on("initializeClutter", function(clutter) {
+        for (let id in clutter) {
+            document.getElementById(id).style.left = clutter[id].x + "px";
+            document.getElementById(id).style.top = clutter[id].y + "px";
+        }
+    });
+
+    socket.on("updateMatchTimer", function(matchDuration) {
+        matchTime = matchDuration;
+        document.getElementById("timer").innerHTML = matchDuration + "";
+    });
     
     clutterElements.forEach(function(element) {
         element.addEventListener('dragstart', function(e) {
-            e.dataTransfer.setDragImage(element, 35, 35);
-            e.dataTransfer.dropEffect = 'move';
+            if(matchTime > 0) {
+                e.dataTransfer.setDragImage(element, 35, 35);
+                e.dataTransfer.dropEffect = 'move';
+            }
         });
 
         element.addEventListener("drag", function(e) {
-            var x = e.clientX - (window.innerWidth-1280)/2 - 35;
-            var y = e.clientY - 100 - 35;
-            var clutterId = e.target.id;
+            if(matchTime > 0) {
+                var x = e.clientX - (window.innerWidth-1280)/2 - 35;
+                var y = e.clientY - 100 - 35;
+                var clutterId = e.target.id;
 
-            if(x > 0 && y > 0) {
-                socket.emit("dragging", {clutterId, x, y});
+                if(x > 0 && y > 0) {
+                    socket.emit("dragging", {clutterId, x, y});
+                }
             }
         });
         
         element.addEventListener('dragend', function(e) {
-            var x = e.clientX - (window.innerWidth-1280)/2 - 35;
-            var y = e.clientY - 100 - 35;
-            var clutterId = e.target.id;
+            if(matchTime > 0) {
+                var x = e.clientX - (window.innerWidth-1280)/2 - 35;
+                var y = e.clientY - 100 - 35;
+                var clutterId = e.target.id;
 
-            var arenaTop = 0;
-            var arenaBottom = 660;
-            var arenaLeft = 0;
-            var arenaRight = 1280;
+                var arenaTop = 0;
+                var arenaBottom = 660;
+                var arenaLeft = 0;
+                var arenaRight = 1280;
 
-            y = (y < arenaTop + 45)? arenaTop + 10: y;
-            y = (y > arenaBottom - 70)? arenaBottom - 70 : y;
-            x = (x < arenaLeft)? arenaLeft : x;
-            x = (x > arenaRight - 70)? arenaRight - 70 : x; 
-            element.style.left = x + "px";
-            element.style.top = y + "px";
+                y = (y < arenaTop + 45)? arenaTop + 10: y;
+                y = (y > arenaBottom - 70)? arenaBottom - 70 : y;
+                x = (x < arenaLeft)? arenaLeft : x;
+                x = (x > arenaRight - 70)? arenaRight - 70 : x; 
+                element.style.left = x + "px";
+                element.style.top = y + "px";
 
-            socket.emit("drop", {clutterId, x, y});
+                socket.emit("drop", {clutterId, x, y});
+            }
         });
 
     });
